953839f8f5f8d5b60a5f0eadb18a21201dc3bf02
“C_SF” <1464103425@qq.com>
2022-05-25 22:31:25 +0800

    添加vue-loader

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+//在vue-loader中拿到VueLoaderPlugin函数
+const { VueLoaderPlugin } = require('vue-loader')
+
+const config = {
+  entry: "./index.ts",
+  output: {
+    path: path.resolve(__dirname, "lib"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.less$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "less-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+      {
+        //转换文件格式
+        test: /\.vue$/,
+        //文件打包方式
+        use: ["vue-loader"]
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
